b5dd1de292353dc78938e77bf8fa9662661d74a8
bdsa <bdsa@server.fake>
2013-12-15 00:54:37 +0000

    login and logout functionality

--- mercury/urls.py
+++ mercury/urls.py
@@ -10,7 +10,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'mercury.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', ContactIndexView.as_view(), name='contact_index'),
+    url(r'^$', ContactIndexView.as_view(), name='index'),
     url(r'^contacts/', include('nexus.urls', namespace="nexus")),
+    url(r'^contact/', include('nexus.urls', namespace="nexus")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'nexus/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': 'index'}, name='logout'),
 )
--- nexus/views.py
+++ nexus/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 from nexus.models import Contact
 
 class ContactIndexView(generic.ListView):
     template_name = 'nexus/index.html'
     model = Contact
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ContactIndexView, self).dispatch(*args, **kwargs)
     
 class ContactView(generic.DetailView):
     model = Contact
     template_name = 'nexus/contact_detail.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ContactView, self).dispatch(*args, **kwargs)
